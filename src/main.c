903ea6787fbd5a676b2d29e82562a43088aadf66
ThiagoManguinho <tmrs@cesar.school>
2024-11-06 15:40:47 -0300

    atualizando fotos e código

--- src/main.c
+++ src/main.c
@@ -1,43 +1,43 @@
 #include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 #if defined(PLATFORM_WEB)
 	#include <emscripten/emscripten.h>
 #endif
 
-//----------------------------------------------------------------------------------
-// Some Defines
-//----------------------------------------------------------------------------------
-#define NUM_MAX_ENEMIES 50
-#define FIRST_WAVE 15
-#define SECOND_WAVE 25
-#define THIRD_WAVE 30
+//A onda seria por pontuação ( Onda 1 -  )
+//A jeitar geração de obstáculos por onda
+#define NUM_MAX_ENEMIES 20
+#define FIRST_WAVE 7
+#define SECOND_WAVE 15
+#define THIRD_WAVE 20
 
-//----------------------------------------------------------------------------------
-// Types and Structures Definition
-//----------------------------------------------------------------------------------
 typedef enum { FIRST = 0, SECOND, THIRD } OndaObstaculo;
 typedef enum { MENU = 0, PLAY, GAME_OVER } GameState;
 
 typedef struct Barco{
 	Rectangle rec;
 	Vector2 speed;
-	Color color;
 } Barco;
 
-
-//Sugestão 2 structs para cada obstávulo
 typedef struct Obstaculo{
 	Rectangle rec;
 	Vector2 speed;
 	bool active;
-	Color color;
+	Color cor;
 } Obstaculo;
 
-//------------------------------------------------------------------------------------
-// Global Variables Declaration
-//------------------------------------------------------------------------------------
-static const int screenWidth = 800;
-static const int screenHeight = 450;
+typedef struct ListObj{
+	Obstaculo obstaculo;
+	struct ListObj *prox;
+} ListObj;
+
+Obstaculo *head = NULL;
+Obstaculo *tail = NULL;
+
+static const int screenWidth = 1000;
+static const int screenHeight = 749;
 
 static GameState currentGameState = MENU;
 
@@ -59,37 +59,27 @@ Texture2D Barcotextura;
 Texture2D CenarioTexture;
 Texture2D MenuInicial;
 
-//------------------------------------------------------------------------------------
-// Module Functions Declaration (local)
-//------------------------------------------------------------------------------------
-static void InitGame(void);         // Initialize game
-static void UpdateGame(void);       // Update game (one frame)
-static void DrawGame(void);         // Draw game (one frame)
-static void UnloadGame(void);       // Unload game
-static void UpdateDrawFrame(void);  // Update and Draw (one frame)
+static void InitGame(void);
+static void UpdateGame(void);
+static void DrawGame(void);
+static void UnloadGame(void);
+static void UpdateDrawFrame(void);
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void){
 	InitWindow(screenWidth, screenHeight, "classic game: space invaders");
-
 	InitGame();
+	#if defined(PLATFORM_WEB)
+		emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+	#else
+		SetTargetFPS(60);
+		while (!WindowShouldClose()){
+			UpdateDrawFrame();
+		}
+	#endif
+		UnloadGame();
+		CloseWindow();
 
-#if defined(PLATFORM_WEB)
-	emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
-#else
-	SetTargetFPS(60);
-
-	while (!WindowShouldClose()){
-		UpdateDrawFrame();
-	}
-#endif
-
-	UnloadGame();
-	CloseWindow();
-
-	return 0;
+		return 0;
 }
 
 // Initialize game variables
@@ -103,28 +93,46 @@ void InitGame(void){
 	smooth = false;
 	wave = FIRST;
 	activeEnemies = FIRST_WAVE;
-	score = 100;
+	//Pontuação quando chega no final da tela ( 10 em 10 )
+	score = 0;
 	alpha = 0;
 
 	// Initialize player
-	barco.rec.x =  20;
-	barco.rec.y = (screenHeight/2) + 50;
+	barco.rec.x = 20;
+	barco.rec.y = 50;
 	barco.rec.width = Barcotextura.width;
-	barco.rec.height = Barcotextura.height - 25;
+	barco.rec.height = Barcotextura.height - 30;
 	barco.speed.x = 4;
 	barco.speed.y = 4;
 
 	// Initialize enemies
-	for (int i = 0; i < NUM_MAX_ENEMIES; i++){
-		obstaculo[i].rec.width = 15;
-		obstaculo[i].rec.height = 15;
-		obstaculo[i].rec.x = GetRandomValue(screenWidth, screenWidth + 1000);
-		obstaculo[i].rec.y = GetRandomValue(screenHeight/2, screenHeight - obstaculo[i].rec.height);
-		obstaculo[i].speed.x = 5;
-		obstaculo[i].speed.y = 5;
-		obstaculo[i].active = true;
-		obstaculo[i].color = GRAY;
+	for (int i = 0; i < 7; i++){
+		inserir(&head, &tail);
 	}
+}
+
+//insere um novo obstaculo na lista
+void inserir(ListObj **head, ListObj **tail) {
+  ListObj *novo = (ListObj *) malloc(sizeof(ListObj));
+  if(novo != NULL){
+    novo->obstaculo.rec.width = 15;
+    novo->obstaculo.rec.height = 15;
+    novo->obstaculo.rec.x = GetRandomValue(screenWidth, screenWidth + 1000);
+    novo->obstaculo.rec.y = GetRandomValue(470, 700);
+	novo->obstaculo.speed.x = 5;
+	novo->obstaculo.speed.y = 5;
+	novo->obstaculo.active = true;
+	novo->obstaculo.cor = DARKGRAY;
+	novo->prox = *head;
+    if(*head==NULL){
+		*head = *tail = novo;
+		(*tail)->prox = novo;
+    }else{
+		(*tail)->prox = novo;
+		novo->prox = *head;
+		*tail = novo;
+    }
+  }
 }
 
 // Desenhando o Menu
@@ -134,143 +142,103 @@ void DrawMenu(void) {
     DrawTexture(MenuInicial, 0, 0, WHITE);
     
     // Título do Jogo (Posicionado no canto superior esquerdo)
-    DrawText("PETROLÂNDIA Adventure", 20, 20, 45, DARKBLUE);
+    DrawText("PETROLÂNDIA ADVENTURE", 20, 20, 45, DARKBLUE);
     
     // Texto de instrução (Posicionado abaixo do título)
-    DrawText("ENTER PARA INICIAR", 20, 100, 25, DARKGRAY);
-    DrawText("ESC PARA SAIR", 20, 150, 25, DARKGRAY);
+    DrawText("ENTER PARA INICIAR", 20, 100, 30, DARKBLUE);
+    DrawText("ESC PARA SAIR", 20, 150, 30, DARKBLUE);
     
     EndDrawing();
-}
-
+} 
 
 void UpdateMenu(void) {
     if (IsKeyPressed(KEY_ENTER)) {
-        // Começa o jogo
         currentGameState = PLAY;
-        InitGame();  // Inicializa o jogo (barco, inimigos, etc.)
+        InitGame();
     }
-    
     if (IsKeyPressed(KEY_ESCAPE)) {
-        // Fecha o jogo
         CloseWindow();
     }
 }
 
-
-// Update game (one frame)
 void UpdateGame(void){
+	ListObj *atual = head;
+
 	if (!gameOver){
-		
-		switch (wave){
-			case FIRST:{
-				if (!smooth){
-					alpha += 0.02f;
+		//Mostra na tela "INICIO DA AVENTURA"
+		if (!smooth){
+			alpha += 0.02f;
+			if (alpha >= 1.0f) smooth = true;
+		}
+		if (smooth) alpha -= 0.02f;
 
-					if (alpha >= 1.0f) smooth = true;
+		if (enemiesKill == activeEnemies){
+			enemiesKill = 0;
+			do{
+				if (!atual->obstaculo.active) {
+                	atual->obstaculo.active = true;
 				}
+				atual = atual->prox;
+			} while (atual != head);
 
-				if (smooth) alpha -= 0.02f;
-
-				if (enemiesKill == activeEnemies){
-					enemiesKill = 0;
-					for (int i = 0; i < activeEnemies; i++){
-						if (!obstaculo[i].active) obstaculo[i].active = true;
-					}
-
-					activeEnemies = SECOND_WAVE;
-					wave = SECOND;
-					smooth = false;
-					alpha = 0.0f;
-				}
-			} break;
-			case SECOND:{
-				if (!smooth)
-				{
-					alpha += 0.02f;
-
-					if (alpha >= 1.0f) smooth = true;
-				}
-
-				if (smooth) alpha -= 0.02f;
-
-				if (enemiesKill == activeEnemies)
-				{
-					enemiesKill = 0;
-
-					for (int i = 0; i < activeEnemies; i++)
-					{
-						if (!obstaculo[i].active) obstaculo[i].active = true;
-					}
-
-					activeEnemies = THIRD_WAVE;
-					wave = THIRD;
-					smooth = false;
-					alpha = 0.0f;
-				}
-			} break;
-			case THIRD:{
-				if (!smooth)
-				{
-					alpha += 0.02f;
-
-					if (alpha >= 1.0f) smooth = true;
-				}
-
-				if (smooth) alpha -= 0.02f;
-
-				if (enemiesKill == activeEnemies) victory = true;
-
-			} break;
-			default: break;
+			activeEnemies = SECOND_WAVE;
+			wave = SECOND;
+			smooth = false;
+			alpha = 0.0f;
 		}
 
-		// Player movement
+		atual = head;
+
+		//Movimentação
 		if (IsKeyDown(KEY_D)) barco.rec.x += barco.speed.x;
 		if (IsKeyDown(KEY_A)) barco.rec.x -= barco.speed.x;
 		if (IsKeyDown(KEY_W)) barco.rec.y -= barco.speed.y;
 		if (IsKeyDown(KEY_S)) barco.rec.y += barco.speed.y;
 
-		// Player collision with enemy
-		for (int i = 0; i < activeEnemies; i++){
-			if (CheckCollisionRecs(barco.rec, obstaculo[i].rec)){
-				score -= 5;
-				obstaculo[i].active = false;
-				if (score == 0){
-					score = 0;
-					gameOver = true;
-				}
-			}
+		//Colisão
+		do {
+            if (atual->obstaculo.active && CheckCollisionRecs(barco.rec, atual->obstaculo.rec)) {
+                score -= 10;
+                atual->obstaculo.active = false;
+                if (score == 0) {
+                    gameOver = true;
+                }
+            }
+
+            if (!atual->obstaculo.active) {
+                atual->obstaculo.active = true;
+                atual->obstaculo.rec.x = GetRandomValue(screenWidth, screenWidth + 1000);
+                atual->obstaculo.rec.y = GetRandomValue(470, 700);
+            }
+
+            if (atual->obstaculo.active) {
+                atual->obstaculo.rec.x -= atual->obstaculo.speed.x;
+                if (atual->obstaculo.rec.x < 0) {
+					score += 10;
+                    atual->obstaculo.rec.x = GetRandomValue(screenWidth, screenWidth + 1000);
+                    atual->obstaculo.rec.y = GetRandomValue(470, 700);
+                }
+            }
+
+            atual = atual->prox;
+        } while (atual != head);
+
+		//Comportamento Parede
+		if (barco.rec.x <= 0){
+			barco.rec.x = 0;
 		}
-
-		//Comportamento Objeto
-		for (int i = 0; i < activeEnemies; i++){
-
-			if (!obstaculo[i].active){
-				obstaculo[i].active = true;
-				obstaculo[i].rec.x = GetRandomValue(screenWidth, screenWidth + 1000);
-				obstaculo[i].rec.y = GetRandomValue(0, screenHeight - obstaculo[i].rec.height);
-			}
-
-			if (obstaculo[i].active){
-				obstaculo[i].rec.x -= obstaculo[i].speed.x;
-				if (obstaculo[i].rec.x < 0){
-					obstaculo[i].rec.x = GetRandomValue(screenWidth, screenWidth + 1000);
-					obstaculo[i].rec.y = GetRandomValue(0, screenHeight - obstaculo[i].rec.height);
-				}
-			}
+		if (barco.rec.x + barco.rec.width >= screenWidth){
+			barco.rec.x = screenWidth - barco.rec.width;
 		}
-
-		// Wall behaviour
-		if (barco.rec.x <= 0) barco.rec.x = 0;
-		if (barco.rec.x + barco.rec.width >= screenWidth) barco.rec.x = screenWidth - barco.rec.width;
-		if (barco.rec.y <= 0) barco.rec.y = 0;
-		if (barco.rec.y + barco.rec.height >= screenHeight) barco.rec.y = screenHeight - barco.rec.height;
+		if (barco.rec.y <= 450){
+			barco.rec.y = 450;
+		}
+		if (barco.rec.y + barco.rec.height >= 730){
+			barco.rec.y = 730 - barco.rec.height;
+		}
 	}
-
 	else{
-		if (IsKeyPressed(KEY_ENTER))
-		{
+		if (IsKeyPressed(KEY_ENTER)){
 			InitGame();
 			gameOver = false;
 		}
@@ -280,55 +248,53 @@ void UpdateGame(void){
 // Draw game (one frame)
 void DrawGame(void){
 	BeginDrawing();
+	DrawTexture(CenarioTexture, 0, 0, WHITE);
 
-		DrawTexture(CenarioTexture, 0, 0, WHITE);
+	if (!gameOver){
+		DrawTexture(Barcotextura, (int)barco.rec.x, (int)barco.rec.y, WHITE);
 
-		if (!gameOver){
-			DrawTexture(Barcotextura, (int)barco.rec.x, (int)barco.rec.y, WHITE);
+		if (wave == FIRST) DrawText("INICIO DA AVENTURA", screenWidth/2 - MeasureText("INICIO DA AVENTURA", 40)/2, screenHeight/2 - 40, 40, Fade(BLACK, alpha));
 
-			//Ondas Obstaculos - MUDAR POR TEMPO
-			if (wave == FIRST) DrawText("FIRST WAVE", screenWidth/2 - MeasureText("FIRST WAVE", 40)/2, screenHeight/2 - 40, 40, Fade(BLACK, alpha));
-			else if (wave == SECOND) DrawText("SECOND WAVE", screenWidth/2 - MeasureText("SECOND WAVE", 40)/2, screenHeight/2 - 40, 40, Fade(BLACK, alpha));
-			else if (wave == THIRD) DrawText("THIRD WAVE", screenWidth/2 - MeasureText("THIRD WAVE", 40)/2, screenHeight/2 - 40, 40, Fade(BLACK, alpha));
-
-			for (int i = 0; i < activeEnemies; i++){
-				if (obstaculo[i].active) DrawRectangleRec(obstaculo[i].rec, obstaculo[i].color);
+		ListObj *atual = head;
+		do{
+			if (atual->obstaculo.active){
+				DrawRectangleRec(atual->obstaculo.rec, atual->obstaculo.cor);
 			}
+			atual = atual->prox;
+		}while (atual != head);
 
-			//Pontuação
-			DrawText(TextFormat("%04i", score), 20, 20, 40, RED);
+		//Pontuação
+		DrawText(TextFormat("%04i", score), 20, 20, 40, RED);
 
-			//Menssagem Vitoria - MUDAR POR TEMPO
-			if (victory) DrawText("YOU WIN", screenWidth/2 - MeasureText("YOU WIN", 40)/2, screenHeight/2 - 40, 40, BLACK);
-		}
-		else {
-		        DrawText("PRESS ENTER TO PLAY AGAIN", screenWidth / 2 - MeasureText("PRESS ENTER TO PLAY AGAIN", 20) / 2, screenHeight / 2 + 20, 20, DARKGRAY);
-		}EndDrawing();
+		//Menssagem Vitoria - MUDAR POR TEMPO
+		if (victory) DrawText("YOU WIN", screenWidth/2 - MeasureText("YOU WIN", 40)/2, screenHeight/2 - 40, 40, BLACK);
+	}
+	else DrawText("PRESS [ENTER] TO PLAY AGAIN", GetScreenWidth()/2 - MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20)/2, GetScreenHeight()/2 - 50, 20, BLACK);
+
+	EndDrawing();
 }
 
 // Unload game variables
 void UnloadGame(void){
-	// Libere a textura
 	UnloadTexture(Barcotextura);
 	UnloadTexture(CenarioTexture);
 	UnloadTexture(MenuInicial);
 }
 
-// Update and Draw (one frame)
 void UpdateDrawFrame(void) {
     switch (currentGameState) {
         case MENU:
-            UpdateMenu();  // Atualiza o menu
-            DrawMenu();    // Desenha o menu
+            UpdateMenu();
+            DrawMenu();
             break;
         case PLAY:
-            UpdateGame();  // Atualiza o jogo (movimentação, inimigos, etc.)
-            DrawGame();    // Desenha o jogo
+            UpdateGame();
+            DrawGame();
             break;
         case GAME_OVER:
-            DrawGame();    // Desenha a tela de game over (pode ser a mesma de jogo, mas com uma mensagem)
+            DrawGame();
             if (IsKeyPressed(KEY_ENTER)) {
-                currentGameState = MENU;  // Volta para o menu
+                currentGameState = MENU;
             }
             break;
         default:
